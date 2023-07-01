5f722724952af05cdee9501860c4fc3d536e701e
leviathan095 <138323080+leviathan095@users.noreply.github.com>
2023-07-01 22:04:01 +0300

    Update mushroomgraphs.py

--- mushroomgraphs.py
+++ mushroomgraphs.py
@@ -3,18 +3,27 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-mushroom_data= pd.read_csv('C:\codecademy\Exploring Mushrooms\mushroom_data.csv')
-print(mushroom_data.head())
+df= pd.read_csv('C:\codecademy\Exploring Mushrooms\mushroom_data.csv')
+print(df.head())
 
 #columns
-for index in range(0,len(mushroom_data['Bruises'])):
-    mushroom_data['Bruises'][index]=str(mushroom_data['Bruises'][index])
-columns = mushroom_data.columns.tolist()
+for index in range(0,len(df['Bruises'])):
+    df['Bruises'][index]=str(df['Bruises'][index])
+columns = df.columns.tolist()
 for column in columns:
-    sns.countplot(x=column,data=mushroom_data,order=mushroom_data[column].value_counts().index)
-    #print(column)
-    plt.xticks(rotation=30,fontsize=10)
-    plt.xlabel(column,fontsize=12)
-    plt.title(column + ' Value Counts')
-    plt.show()
-    plt.clf()
+    if column in ['Veil Type','Veil Color']:
+        print('The data is not informative for {}'.format(column))
+    elif len(df[column].unique())<4:
+        plt.pie(df[column].value_counts(),labels=df[column].value_counts().index)
+        plt.axis('equal')
+        plt.title(column + ' Value Counts')
+        plt.show()
+        plt.clf()
+    else:
+        sns.countplot(x=column,data=df,order=df[column].value_counts().index)
+        plt.xticks(rotation=30,fontsize=10)
+        plt.xlabel(column,fontsize=12)
+        plt.title(column + ' Value Counts')
+        plt.show()
+        plt.clf()
+        
